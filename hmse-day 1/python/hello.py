e14285a0bca1c722b23b026b9191eaddb04cdf90
raniaa555 <dwiniarizki@gmail.com>
2026-09-20 17:16:18 +0700

    Add hello.py with various Python data types and operations

---
+++ hmse-day 1/python/hello.py
@@ -0,0 +1,104 @@
+print("hello world")
+
+# ini komentar satu baris
+
+"ini komentar dua baris"
+print("hello world") #ini komentar samping
+
+
+#Tipe data boolean
+print(True)
+
+#Tipe data string
+print("Ayo belajar Python")
+print("Belajar Python sangat mudah")
+
+#Tipe data integer
+print(20)
+
+#Tipe data float
+print(3.14)
+
+#Tipe data hexadecimal
+print(0x9a)
+
+#Tipe data complex
+print(5j)
+
+#Tipe data list
+print([1,2,3])
+print(['satu', 'dua', 'tiga'])
+
+#Tipe data tuple
+print((1,2,3))
+print(('satu', 'dua', 'tiga'))
+
+#Tipe data set
+print({1,2,3})
+print({'satu', 'dua', 'tiga'})
+
+#Tipe data dictionary
+print({"nama":"Budi", "umur":20})
+
+print(type(3.14))
+print(type("Hello"))
+
+biodata = {"nama":"Budi", "umur":20}
+print(biodata)
+
+print(type(biodata))
+
+FirstName = "Budi"
+Lastame = "Susanto"
+umur = 20
+hobi = "Berenang"
+name = FirstName + " " + Lastame
+
+print("nama saya",name,", Saya berusia",umur,"tahun. Hobi saya adalah",hobi)
+
+apel = 7
+jeruk = 9
+buah = apel + jeruk
+print(buah)
+
+print(13+2)
+
+# pengurangan
+hutang = 10000
+bayar = 5000
+sisa = hutang - bayar
+print("Sisa hutang Anda adalah ", sisa)
+
+# perkalian
+p = 15
+l = 8
+luas = p*l
+print(luas)
+
+# pembagian
+kue = 16
+anak = 4
+bagian = kue / anak
+print("Setiap anak akan mendapatkan kue sebanyak ", bagian)
+
+# sisa bagi / modulus
+b1 = 14
+b2 = 5
+sisa = b1 % b2
+print(sisa)
+
+# pembagian bulat
+print(10//3)
+
+# pangkat
+bilangan1 = 8
+bilangan2 = 2
+hasil = bilangan1 ** bilangan2
+print(hasil)
+
+print("PROGRAM PENJUMLAHAN")
+a = int(input("Masukkan angka pertama : "))
+b = int(input("Masukkan angka kedua : "))
+print("Jumlah =", a+b)
+
+a=c, b=c, a=b
