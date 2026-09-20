18f61ddfe687aee4cd7e40f99b463ad41c3a200b
raniaa555 <dwiniarizki@gmail.com>
2026-09-20 18:30:30 +0700

    Add second method for swapping variables in input.py
    Added a second method for swapping variables using a temporary variable.

--- hmse-day 1/python/input.py
+++ hmse-day 1/python/input.py
@@ -1,7 +1,19 @@
+# cara 1
 a = input("angka pertama")
 b = input("angka kedua")
 
 a,b = b,a
+print("Hasil setelah ditukar")
+print("a =", a) 
+print("b =", b)
+
+# cara 2
+a = input("angka pertama")
+b = input("angka kedua")
+
+c = a
+a = b
+b = c
 print("Hasil setelah ditukar")
 print("a =", a) 
 print("b =", b)
